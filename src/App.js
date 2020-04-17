dc8faab617877ccccf86e17abccfd0a8079d693b
Jens Duray <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-04-17 15:30:29 +0200

    work once for menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./script2.js";
 import "./style.css";
 import "./styles.css";
 
---
+++ src/script2.js
@@ -0,0 +1,9 @@
+import $ from "jquery"; // elements
+var $page = $(".page");
+
+$(".menu_toggle").on("click", function() {
+  $page.toggleClass("shazam");
+});
+$(".content").on("click", function() {
+  $page.removeClass("shazam");
+});
